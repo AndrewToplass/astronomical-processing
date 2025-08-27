063629f02b944de68a5eb4863d6d720b83d07343
AndrewToplass <160808664+AndrewToplass@users.noreply.github.com>
2025-08-27 14:52:43 +0800

    Add mathematical calculations to AstroMath library

--- AstroMath/Formula.cs
+++ AstroMath/Formula.cs
@@ -1,5 +1,28 @@
 ﻿namespace AstroMath;
 
-public static class Formula
+public static class Calculations
 {
+    private const double speedOfLight = 299_792_458.0; // m/s
+
+    public static double StarVelocity(double observed, double rest)
+    {
+        return speedOfLight * (observed - rest) / rest;
+    }
+
+    public static double StarDistance(double parallaxAngle)
+    {
+        return 1.0 / parallaxAngle;
+    }
+    
+    public static double DegreesCelsiusToKelvin(double degreesC)
+    {
+        const double celsiusOffset = 273.15;
+        return degreesC + celsiusOffset;
+    }
+
+    public static double BlackholeEventHorizon(double massKg)
+    {
+        const double gravitationalConstant = 6.67430e-11; // m^3 kg^-1 s^-2
+        return 2 * gravitationalConstant * massKg / (speedOfLight * speedOfLight);
+    }
 }
